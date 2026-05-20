8681a8a03b966c2d5840cd611dd9a2f95031954f
shaima <shaima.anvar@gmail.com>
2026-05-20 17:27:39 -0400

    Implemented valid login automation test

--- automation-saucedemo/src/test/java/tests/LoginTest.java
+++ automation-saucedemo/src/test/java/tests/LoginTest.java
@@ -1,16 +1,22 @@
 package tests;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
-
 public class LoginTest {
     @Test
-    public void launchWebsite(){
+    public void validLoginTest() {
+
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.saucedemo.com");
-        System.out.println(driver.getTitle());
-        //driver.quit();
-
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        String currentUrl = driver.getCurrentUrl();
+        Assert.assertTrue(currentUrl.contains("inventory"));
+        driver.quit();
     }
 }
+
