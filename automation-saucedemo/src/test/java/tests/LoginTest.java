a5b6c08086a1abe3e2fe21c3005ca93b2a60a040
shaima <shaima.anvar@gmail.com>
2026-05-20 18:18:50 -0400

    Refactored login automation using Page Object Model

--- automation-saucedemo/src/test/java/tests/LoginTest.java
+++ automation-saucedemo/src/test/java/tests/LoginTest.java
@@ -1,22 +1,21 @@
 package tests;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.LoginPage;
+
 public class LoginTest {
+
     @Test
     public void validLoginTest() {
-
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.saucedemo.com");
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        driver.findElement(By.id("login-button")).click();
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login("standard_user", "secret_sauce");
         String currentUrl = driver.getCurrentUrl();
         Assert.assertTrue(currentUrl.contains("inventory"));
         driver.quit();
     }
 }
-
